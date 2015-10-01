c6e8baa7fc34cf9e1ccc1870861670815de419a9
Michael Pesavento <mike@peztek.com>
2015-10-01 01:49:54 -0700

    MuseConnect is now threaded, uses attributes cleanly, and maintains deque buffers inside the object to be pulled when needed

--- museEEG/museconnect.py
+++ museEEG/museconnect.py
@@ -1,10 +1,22 @@
+"""
+MuseConnect
+reads OSC input from muse-io and creates queues to hold each value, pulled exernally when needed
+
+author: Mike Pesavento
+date: 2015.09.30
+"""
+
+
 import argparse
+import threading
+
+from collections import deque
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
 
-class MuseConnect:
+class MuseConnect(object):
     """
     creates osc server and handlers to read data from Interaxon Muse headset
 
@@ -12,47 +24,104 @@ class MuseConnect:
     https://sites.google.com/a/interaxon.ca/muse-developer-site/download
 
     load muse OSC output in command line with:
-        muse-io --preset 14 --osc osc.udp://localhost:5000
-        muse-io --preset 14 --osc osc.udp://localhost:5000 --device Muse-7C56
-    """
-    def __init__(self, ipAddress="127.0.0.1", port=5000):
+        muse-io --preset 14 --osc osc.udp://localhost:5000 --osc-timestamp
+        muse-io --preset 14 --osc osc.udp://localhost:5000 --osc-timestamp --device Muse-7C56
+    The "--osc-timestamp" adds on 2 extra elements to each OSC message, containing
+    a long int for the unix epoch time, and another int for the milliseconds
 
+    Each member that catches information from the muse-io OSC output puts it in a deque object after
+    some basic analysis (eg averaging the frontal sensors only)
+
+    NOTE: this class is very poorly written, not pythonic at all. Given the number of
+    OSC paths that we are catching and the self similarity of them, I could be doing this much
+    more efficiently
+    """
+    def __init__(self, ipAddress="127.0.0.1", port=5000, verbose=True):
+        self.verbose = verbose  # if true, print all caught OSC packet analysis products
+
+        self.connected = False
         self.oscDispatcher = dispatcher.Dispatcher()
         # oscDispatcher.map("/debug", print)
         self.oscDispatcher.map("/muse/batt", self.battery_handler, "battery")
         self.oscDispatcher.map("/muse/elements/touching_forehead", self.touchingforehead_handler, "touchingforehead")
         self.oscDispatcher.map("/muse/elements/horseshoe", self.horseshoe_handler, "horseshoe")
-        # self.oscDispatcher.map("/muse/elements/delta_absolute", self.eeg_delta_handler, "delta_absolute")
-        # self.oscDispatcher.map("/muse/elements/theta_absolute", self.eeg_theta_handler, "theta_absolute")
-        # self.oscDispatcher.map("/muse/elements/alpha_absolute", self.eeg_alpha_handler, "alpha_absolute")
-        # self.oscDispatcher.map("/muse/elements/beta_absolute", self.eeg_beta_handler, "beta_absolute")
-        # self.oscDispatcher.map("/muse/elements/gamma_absolute", self.eeg_gamma_handler, "gamma_absolute")
 
-        self.oscDispatcher.map("/muse/elements/delta_relative", self.eeg_delta_relative_handler, "delta_relative")
-        self.oscDispatcher.map("/muse/elements/theta_relative", self.eeg_theta_relative_handler, "theta_relative")
-        self.oscDispatcher.map("/muse/elements/alpha_relative", self.eeg_alpha_relative_handler, "alpha_relative")
-        self.oscDispatcher.map("/muse/elements/beta_relative", self.eeg_beta_relative_handler, "beta_relative")
-        self.oscDispatcher.map("/muse/elements/gamma_relative", self.eeg_gamma_relative_handler, "gamma_relative")
+        self.oscDispatcher.map("/muse/elements/delta_absolute", self.eeg_bandpower_handler, "delta_absolute")
+        self.oscDispatcher.map("/muse/elements/theta_absolute", self.eeg_bandpower_handler, "theta_absolute")
+        self.oscDispatcher.map("/muse/elements/alpha_absolute", self.eeg_bandpower_handler, "alpha_absolute")
+        self.oscDispatcher.map("/muse/elements/beta_absolute", self.eeg_bandpower_handler, "beta_absolute")
+        self.oscDispatcher.map("/muse/elements/gamma_absolute", self.eeg_bandpower_handler, "gamma_absolute")
 
-        self.battery = None
-        self.touchingforehead = None
-        self.horseshoe = None
-        self.delta_relative = None
-        self.theta_relative = None
-        self.alpha_relative = None
-        self.beta_relative = None
-        self.gamma_relative = None
+        self.oscDispatcher.map("/muse/elements/delta_relative", self.eeg_bandpower_handler, "delta_relative")
+        self.oscDispatcher.map("/muse/elements/theta_relative", self.eeg_bandpower_handler, "theta_relative")
+        self.oscDispatcher.map("/muse/elements/alpha_relative", self.eeg_bandpower_handler, "alpha_relative")
+        self.oscDispatcher.map("/muse/elements/beta_relative", self.eeg_bandpower_handler, "beta_relative")
+        self.oscDispatcher.map("/muse/elements/gamma_relative", self.eeg_bandpower_handler, "gamma_relative")
 
-        self.delta_absolute = None
-        self.theta_absolute = None
-        self.alpha_absolute = None
-        self.beta_absolute = None
-        self.gamma_absolute = None
+        # each of these should be an empty queue,
+        # where each element holds a tuple of (timestamp, value)
+        # get all values from the queue with (times, values) = muse.popValue()
+        self.battery = deque()
+        self.touchingforehead = deque()
+        self.leadon = None
+        self.horseshoe = deque()
+        self.currentSensorState = None  # hold just the most recent value from horseshoe
 
-        self.oscServer = osc_server.ThreadingOSCUDPServer((ipAddress, args.port), self.oscDispatcher)
-        print("Serving OSC on {}".format(self.oscServer.server_address))
+        self.delta_absolute = deque()
+        self.theta_absolute = deque()
+        self.alpha_absolute = deque()
+        self.beta_absolute = deque()
+        self.gamma_absolute = deque()
 
-    def battery_handler(self, address, name, chargePercent, fuelgaugeBattVolt, ADCBattVolt, temperature):
+        self.delta_relative = deque()
+        self.theta_relative = deque()
+        self.alpha_relative = deque()
+        self.beta_relative = deque()
+        self.gamma_relative = deque()
+
+        self.oscServer = osc_server.ThreadingOSCUDPServer((ipAddress, port), self.oscDispatcher)
+        self.oscServer.daemon = True
+        print("Muse OSC client running on {}".format(self.oscServer.server_address))
+
+    def start(self):
+        """
+        start the osc server & message handler
+        """
+        self.connected = True
+        # self.oscServer.serve_forever()
+        t = threading.Thread(target=self.oscServer.serve_forever)
+        print("test")
+        t.daemon = True
+        t.start()
+        print("Started Muse OSC reader")
+
+    def shutdown(self):
+        """
+        close the osc server
+        """
+        # do we actually even need this?
+        self.oscServer.shutdown()
+
+    def vprint(self, value):
+        """
+        verbose print, only if verbose is on
+        """
+        if self.verbose:
+            print(value)
+
+    def _timestamp(self, ts, tsms):
+        """
+        return the epoch timestamp in floating point
+        """
+        return ts + float(tsms) / 1e6
+
+    def _averageFront(self, channelValues):
+        """
+        average the front sensor values
+        """
+        return (channelValues[1] + channelValues[2]) / 2.
+
+    def battery_handler(self, address, name, chargePercent, fuelgaugeBattVolt, ADCBattVolt, temperature, ts, tsms):
         """
         gets battery data from Muse, with:
         chargePercent = %/100, range (0-10000)
@@ -61,17 +130,19 @@ class MuseConnect:
         temperature = C, -40 to 125 C
         updates at 0.1 Hz
         """
-        # print("battery:", names, ":", chargePercent, fuelgaugeBattVolt, ADCBattVolt,temperature)
-        print("battery: {}".format(chargePercent / 100.))
-        self.battery = chargePercent / 100.  # return percent charge in floating point
+        # print("battery:", name, ":", chargePercent, fuelgaugeBattVolt, ADCBattVolt, temperature, ts, tsms)
+        self.vprint("battery: {}".format(chargePercent / 100.))
+        element = (self._timestamp(ts, tsms), chargePercent / 100.)
+        self.battery.append(element)  # return percent charge in floating point
 
     def touchingforehead_handler(self, address, name, touchingforehead):
         """
         returns value 1 if touching forehead, 0 if not
         updated at 10 Hz
         """
-        print("touchingforehead: {}".format(touchingforehead))
-        self.touchingforehead = touchingforehead
+        self.vprint("touchingforehead: {}".format(touchingforehead))
+        self.touchingforehead.append(touchingforehead)
+        self.leadon = touchingforehead
 
     def horseshoe_handler(self, address, name, ch1, ch2, ch3, ch4):
         """
@@ -79,71 +150,48 @@ class MuseConnect:
         1 = good, 2 = ok, >=3 bad
         """
         horseshoe = [ch1, ch2, ch3, ch4]
-        print("horseshoe: {}".format(horseshoe))
-        self.horseshoe = horseshoe
+        self.vprint("horseshoe: {}".format(horseshoe))
+        # element = (self.timestamp(ts, tsms), horseshoe)
+        self.horseshoe.append(horseshoe)
+        self.currentSensorState = horseshoe
 
-    def eeg_delta_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontDelta = (ch2 + ch3) / 2
-        print("delta: {}".format(frontDelta))
-        self.delta_absolute = frontDelta
-
-    def eeg_theta_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontTheta = (ch2 + ch3) / 2
-        print("theta: {}".format(frontTheta))
-        self.theta_absolute = frontTheta
-
-    def eeg_alpha_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontAlpha = (ch2 + ch3) / 2
-        print("alpha: {}".format(frontAlpha))
-        self.alpha_absolute = frontAlpha
-
-    def eeg_beta_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontBeta = (ch2 + ch3) / 2
-        print("beta: {}".format(frontBeta))
-        self.beta_absolute = frontBeta
-
-    def eeg_gamma_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontGamma = (ch2 + ch3) / 2
-        print("gamma: {}".format(frontGamma))
-        self.gamma_absolute = frontGamma
-
-    def eeg_delta_relative_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontDelta = (ch2 + ch3) / 2
-        print("delta_relative: {}".format(frontDelta))
-        self.delta_relative = frontDelta
-
-    def eeg_theta_relative_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontTheta = (ch2 + ch3) / 2
-        print("theta_relative: {}".format(frontTheta))
-        self.theta_relative = frontTheta
-
-    def eeg_alpha_relative_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontAlpha = (ch2 + ch3) / 2
-        print("alpha_relative: {}".format(frontAlpha))
-        self.alpha_relative = frontAlpha
-
-    def eeg_beta_relative_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontBeta = (ch2 + ch3) / 2
-        print("beta_relative: {}".format(frontBeta))
-        self.beta_relative = frontBeta
-
-    def eeg_gamma_relative_handler(self, address, name, ch1, ch2, ch3, ch4):
-        frontGamma = (ch2 + ch3) / 2
-        print("gamma_relative: {}".format(frontGamma))
-        self.gamma_relative = frontGamma
-
-    def run(self):
+    def eeg_bandpower_handler(self, address, name, ch1, ch2, ch3, ch4):
         """
-        start the osc server & message handler
+        uses class attributes to append values to the correct attribute queue
         """
-        self.oscServer.serve_forever()
+        values = [ch1, ch2, ch3, ch4]
+        out = self._averageFront(values)
+        self.vprint("{}: {}".format(name[0], out))
+        attr = self.__getattribute__(name[0])
+        attr.append(out)
 
-    def shutdown(self):
+    def popAll(self, name):
         """
-        close the osc server
+        clear the queue, using muse attribute "name", e.g. "alpha_absolute"
         """
-        # do we actually even need this?
-        self.oscServer.shutdown()
+        attr = self.__getattribute__(name)
+        return [attr.popleft() for _i in range(len(attr))]
+
+    def pop(self, name):
+        """
+        get oldest value in attribute with name, e.g. "alpha_absolute"
+        """
+        attr = self.__getattribute__(name)
+        return attr.popleft()
+
+    def get_alpha(self):
+        """
+        the specific function used in Change Your Mind to get
+        the absolute alpha power
+        """
+        return self.popAll("alpha_absolute")
+
+    def getSensorState(self):
+        """
+        return the buffer of sensor states.
+        typically only the last one matters
+        """
+        return self.popAll("horseshoe")
 
 
 if __name__ == "__main__":
@@ -161,12 +209,12 @@ if __name__ == "__main__":
 
     muse = MuseConnect(args.ip, args.port)
 
-    muse.run()
+    muse.start()  # this is a blocking call!!! wtf!!!??
 
     # ## this doesnt actually do anything
     # try :
     #     while 1 :
-    #         time.sleep(5)
+    #         time.sleep(1)
 
     # except KeyboardInterrupt :
     #     print("\nClosing OSCServer.")
